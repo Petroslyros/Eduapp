a2b27cb1ad57a91bbcfc363fde1c2cd8a082eb93
Petros <petrosluros@gmail.com>
2025-07-29 20:25:33 +0300

    models

---
+++ src/main/java/gr/aueb/cf/eduapp/model/Teacher.java
@@ -0,0 +1,39 @@
+package gr.aueb.cf.eduapp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "teachers")
+public class Teacher extends AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String uuid;
+
+    @ColumnDefault("true")  // creates default value on the database
+    private Boolean isActive;
+
+    @OneToOne(cascade = CascadeType.ALL)  // inserting user will insert teacher and vice versa (on service we dont have to save both)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "personal_info_id")
+    private PersonalInfo personalInfo;
+
+    @PrePersist
+    public void initializeUUID() {
+        if (uuid == null) uuid = UUID.randomUUID().toString();
+    }
+}
